e865bfdb7080b8051ccb255ceaa98a549bb373a1
SIX <douglas.six@ext.adeo.com>
2020-11-03 09:50:21 +0100

    Add Insomnia API file

--- src/main/java/com/example/boissons/Boisson.java
+++ src/main/java/com/example/boissons/Boisson.java
@@ -1,31 +1,21 @@
 package com.example.boissons;
 
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
+@Data
 public class Boisson {
     @Id
     private int id;
     private String name;
+    @ManyToOne
+    private TypeBoisson typeBoisson;
 
     public Boisson() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
--- src/main/java/com/example/boissons/TypeBoisson.java
+++ src/main/java/com/example/boissons/TypeBoisson.java
@@ -1,17 +1,22 @@
 package com.example.boissons;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
-import java.util.Set;
+import javax.persistence.OrderBy;
+import java.util.List;
 
 @Entity
+@Data
 public class TypeBoisson {
     @Id
     private int id;
     private String name;
     @OneToMany
     @JoinColumn(name = "type_boisson_id")
-    private Set<Boisson> boissons;
+    @OrderBy("name")
+    private List<Boisson> boissons;
 }
